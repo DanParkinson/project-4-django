e90bda0bdc3a716541a9df85b9efc8e1f848d2e7
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-13 15:43:23 +0000

    Reservations time now limited to opening times in 15 minute intervals

--- reservations/forms.py
+++ reservations/forms.py
@@ -5,6 +5,18 @@ class ReservationForm(forms.Form):
     email = forms.EmailField(label='Email')
     phone_number = forms.CharField(max_length=15, label='Phone Number')
     date = forms.DateField(widget=forms.SelectDateWidget, label='Date')
-    time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}), label='Time')
     number_of_guests = forms.IntegerField(min_value=1, label='Number of Guests')
     special_occasion = forms.CharField(max_length=200, required=False, label='Special Occasion')
+
+    # creates 15 minute intervals for the time selection drop down within open hours
+    TIME_CHOICES = [
+        (f"{hour:02}:{minute:02}", f"{hour:02}:{minute:02}")
+        for hour in range(10, 21) # 10am to 9pm
+        for minute in (0, 15, 30, 45) # 15 minute intervals
+    ]
+    # The actual time data 
+    time = forms.ChoiceField(
+        # Limits users selecting out of hours reservations
+        choices = TIME_CHOICES,
+        label = 'Time'
+         )
