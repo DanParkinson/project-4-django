197abcd12622d4df1175384aa0bd6435699c17d4
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-13 15:44:54 +0000

    Number of Guests capped at 8

--- reservations/forms.py
+++ reservations/forms.py
@@ -5,7 +5,7 @@ class ReservationForm(forms.Form):
     email = forms.EmailField(label='Email')
     phone_number = forms.CharField(max_length=15, label='Phone Number')
     date = forms.DateField(widget=forms.SelectDateWidget, label='Date')
-    number_of_guests = forms.IntegerField(min_value=1, label='Number of Guests')
+    number_of_guests = forms.IntegerField(min_value=1, label='Number of Guests', max_value='8')
     special_occasion = forms.CharField(max_length=200, required=False, label='Special Occasion')
 
     # creates 15 minute intervals for the time selection drop down within open hours
