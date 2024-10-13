9cc9ce65bf3c317d9e3f86a9598b9395e08cf513
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-13 11:12:39 +0000

    Style.css not loading

--- elite/settings.py
+++ elite/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'elite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], # links to templates
+        'DIRS': [TEMPLATES_DIR], # links to templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- home/views.py
+++ home/views.py
@@ -2,3 +2,4 @@ from django.shortcuts import render # render handles HttpResponse
 
 def homepage(request):
     return render(request, 'home/index.html')
+    
