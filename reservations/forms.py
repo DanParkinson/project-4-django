14fe9758bf515385e3c9e59c99d1435176de5673
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-13 13:44:45 +0000

    Typo in reservations/forms.py

--- reservations/forms.py
+++ reservations/forms.py
@@ -5,6 +5,6 @@ class ReservationForm(forms.Form):
     email = forms.EmailField(label='Email')
     Phone_number = forms.CharField(max_length=15, label='PhoneNumber')
     date = forms.DateField(widget=forms.SelectDateWidget, label='Date')
-    time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}, label='Time'))
+    time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}), label='Time')
     number_of_guests = forms.IntegerField(min_value=1, label='Number of Guests')
     special_occasions = forms.CharField(max_length=200, required=False, label='Special Occasion')
