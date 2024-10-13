014d25b4a7669cc015cd219587e5013d2cecf759
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-13 15:51:56 +0000

    Reservation form date set to today

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,12 +1,24 @@
 from django import forms
+from django.utils import timezone
 
 class ReservationForm(forms.Form):
     name = forms.CharField(max_length=100, label='Name')
     email = forms.EmailField(label='Email')
     phone_number = forms.CharField(max_length=15, label='Phone Number')
-    date = forms.DateField(widget=forms.SelectDateWidget, label='Date')
     number_of_guests = forms.IntegerField(min_value=1, label='Number of Guests', max_value='8')
     special_occasion = forms.CharField(max_length=200, required=False, label='Special Occasion')
+
+    # Set date to current day and onwards
+    # get todays date
+    today = timezone.now().date()
+
+    date = forms.DateField(
+        widget=forms.DateInput(attrs={
+            'type':'date',
+            'min': today # set the min date to current day
+        }), 
+        label='Date'
+    )
 
     # creates 15 minute intervals for the time selection drop down within open hours
     TIME_CHOICES = [
