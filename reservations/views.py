d8685fb5bd30c3bd4925cf6a0de5d5e6fb1161e2
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-13 13:36:23 +0000

    reservations/forms.py created

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import ReservationForm # Imports form for reservation
 
 # Create your views here.
+
+def reserve_table(request):
+    if request.method == 'POST': # If user is submitting form
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            # needs stuff for saving to the database
+            return render(request, 'reservations/success.html', {'form' : form})
+    else:
+        form = ReservationForm() # If user not submitting form then they want a form
+    return render(request, 'reservations/reserve_table.html', {'form': form})
