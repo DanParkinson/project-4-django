6cb7695e5b05ae964dc4e5cc9b5f41c5a566f56e
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-13 14:58:52 +0000

    reservations model created and added to admin.py

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Reservations(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    pnune_number = models.CharField(max_length=15)
+    number_of_guests = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+    special_occasions = models.CharField(max_length=200, blank = True)
+
+    def __str__(self):
+        return f"{self.name} - {self.date} at {self.time}"
