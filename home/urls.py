4ebd03ece0b222eecc9dce2092433b13349fee36
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-14 09:56:50 +0000

    Nav bar working for home and make a reservation

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='home'),
+    path('', views.homepage, name='home'), # home page
+     path('reservations/', views.make_reservation, name='make_reservation'),  # Reservation page
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render # render handles HttpResponse
+from reservations.forms import ReservationForm
+
 
 def homepage(request):
     return render(request, 'home/index.html')
+
+def make_reservation(request):
+    form = ReservationForm()  # Create an instance of the form
+    return render(request, 'reservations/reserve_table.html', {'form' : form})
     
