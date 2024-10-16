b6c0f8051c917d102b37ca5c445d109f31cbcab0
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-16 16:24:56 +0000

    home/views.py home_reservation_view created

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render # render handles HttpResponse
 from reservations.forms import ReservationForm
 
+# for loading the home reservation view
+from django.utils import timezone
+from .models import Reservation
+
 
 def homepage(request):
     return render(request, 'home/index.html')
@@ -10,8 +14,17 @@ def make_reservation(request):
     return render(request, 'reservations/reserve_table.html', {'form' : form})
 
 def about_us(request):
-    return render(request, 'home/about_us.html')  # New "About Us" page
+    return render(request, 'home/about_us.html')  #"About Us" page
 
 def menu(request):
-    return render(request, 'home/menu.html')  # New "Menu" page
+    return render(request, 'home/menu.html')  #"Menu" page
+
+# loads the logged in users next reservation onto the homepage
+def home_reservation_view(request):
+    now = timezone.now()
+
+    next_reservation = Reservations.objects.filter(user=request.user,date__gte=now.date()).order_by('date', 'time').first()
+
+    return render(request, 'home.html', {'next_reservation': next_reservation})
+
     
