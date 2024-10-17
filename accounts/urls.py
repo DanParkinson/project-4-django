759b601cdd0f4a86f4106d85d78c143eede17d87
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-10-17 15:21:34 +0000

    users can now update their details

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import account_details
+from .views import account_update
 
 urlpatterns = [
     path('', account_details, name='account'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib import messages
+from django.contrib.auth.forms import UserChangeForm
 
 @login_required # Users must be logged on to access this view
 def account_details(request):
     return render(request, 'account/account_details.html')
 
-class UserUpdateForm(forms.ModelForm):
-    class meta:
+class UserChangeForm(forms.ModelForm):
+    class Meta:
         model = User 
         fields = ['username', 'email']
 
-
+@ login_required
 def account_update(request):
     if request.method == 'POST':
-        form = UserUpdateForm(request.POST, instance=request.user)
+        form = UserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            message.success(request, "Your details have been updated.")
+            messages.success(request, "Your details have been updated.")
             return redirect('account_details')
-        else:
-            form = UserUpdateForm(instance=request.user)
+    else:
+        form = UserChangeForm(instance=request.user)
+
     return render(request,  'account/account_update.html', {'form': form})
